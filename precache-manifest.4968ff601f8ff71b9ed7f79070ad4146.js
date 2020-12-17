a1db85eca1c97c7ba1ee6914e2a57cf74cea6177
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2020-12-17 19:05:25 +0700

    Updates

---
+++ precache-manifest.4968ff601f8ff71b9ed7f79070ad4146.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d93e4ba5b335cd7559bf844e1587c2e",
+    "url": "/page-slider-and-thumbnail/index.html"
+  },
+  {
+    "revision": "f75855a4d6f30ba6c65f",
+    "url": "/page-slider-and-thumbnail/static/css/main.e070eacb.chunk.css"
+  },
+  {
+    "revision": "beaf858d069fb021e05f",
+    "url": "/page-slider-and-thumbnail/static/js/2.e8272949.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/page-slider-and-thumbnail/static/js/2.e8272949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75855a4d6f30ba6c65f",
+    "url": "/page-slider-and-thumbnail/static/js/main.0001e095.chunk.js"
+  },
+  {
+    "revision": "412177841ef0060f3f5f",
+    "url": "/page-slider-and-thumbnail/static/js/runtime-main.287d9f4f.js"
+  },
+  {
+    "revision": "993a93fbc10e11941cd7db236c339c8f",
+    "url": "/page-slider-and-thumbnail/static/media/angry.993a93fb.jpg"
+  },
+  {
+    "revision": "89e8ef972be014f2571311795ec702ce",
+    "url": "/page-slider-and-thumbnail/static/media/awareness.89e8ef97.webp"
+  },
+  {
+    "revision": "a0d0e31856dea3912208fc633ed575c4",
+    "url": "/page-slider-and-thumbnail/static/media/concept.a0d0e318.jpg"
+  },
+  {
+    "revision": "5a5765e96d0ac5034e2c09c1076b53f9",
+    "url": "/page-slider-and-thumbnail/static/media/conclusion.5a5765e9.jpg"
+  },
+  {
+    "revision": "e669f3ef5eb566b539b9c57efb0620a4",
+    "url": "/page-slider-and-thumbnail/static/media/definition.e669f3ef.jpg"
+  },
+  {
+    "revision": "d9495c5ec6ac915a63b4d246bed6dc4a",
+    "url": "/page-slider-and-thumbnail/static/media/demografis.d9495c5e.jpg"
+  },
+  {
+    "revision": "8c907c448691834a10077d5db8b970c9",
+    "url": "/page-slider-and-thumbnail/static/media/gen-z.8c907c44.jpg"
+  },
+  {
+    "revision": "97118979fd1f49c6a68ad8a32fb20a3e",
+    "url": "/page-slider-and-thumbnail/static/media/kerangka2.97118979.jpg"
+  },
+  {
+    "revision": "59c624e6e80babfe43936e83aaed59fe",
+    "url": "/page-slider-and-thumbnail/static/media/latar-belakang.59c624e6.jpg"
+  },
+  {
+    "revision": "a5e43a01e192a99af1a94a69754c2d6a",
+    "url": "/page-slider-and-thumbnail/static/media/method.a5e43a01.jpg"
+  },
+  {
+    "revision": "98ada51e2caf868d747a8a76ae192ec4",
+    "url": "/page-slider-and-thumbnail/static/media/profil.98ada51e.jpeg"
+  },
+  {
+    "revision": "8f26a8d41f7f362600216aef00434441",
+    "url": "/page-slider-and-thumbnail/static/media/puzzles.8f26a8d4.jpg"
+  },
+  {
+    "revision": "421adc7f3340d331fa1c2b7c22b54a5b",
+    "url": "/page-slider-and-thumbnail/static/media/shopee-pasti-ada.421adc7f.jpg"
+  },
+  {
+    "revision": "c6568c3bdb14c32e8b3746585a2bbc8c",
+    "url": "/page-slider-and-thumbnail/static/media/target.c6568c3b.jpeg"
+  },
+  {
+    "revision": "50a88fd3a4089c4b1b96f79af32694bc",
+    "url": "/page-slider-and-thumbnail/static/media/thankyou.50a88fd3.jpg"
+  }
+]);
